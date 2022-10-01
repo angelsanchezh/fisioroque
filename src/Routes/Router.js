b351335dfa459b3360b0607d08720f605ced1d28
angelsanchezh <angelsanchezh@gmail.com>
2022-10-01 10:03:12 -0500

    routes2

---
+++ src/Routes/Router.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import App from '../../src/App';
+
+
+const Rutas = () => {
+
+
+  return (
+
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<App/>}/>
+</Routes>
+</BrowserRouter>
+  )
+}
+
+export default Rutas;
