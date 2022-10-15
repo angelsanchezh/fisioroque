3c2bae80f069f63dd4d462bbe7d6fdd60ea0029d
angelsanchezh <angelsanchezh@gmail.com>
2022-10-15 17:48:30 -0500

    agregacion boton burguer

--- src/Components/Navbaruno/BurguerButton.jsx
+++ src/Components/Navbaruno/BurguerButton.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
 import "./BurguerButton.css";
+import {CgMenuRound} from 'react-icons/cg';
+import {CgCloseO} from 'react-icons/cg';
+import {useState} from 'react';
 
-function BurguerButton(props) {
+const BurguerButton = () => {
+
+  const [open, setOpen] = useState(false);
+
+  const hamburgerIcon = <CgMenuRound className='burguerboton' onClick={()=> setOpen(!open)}/>
+
+  const closeIcon = <CgCloseO className='burguerbotonclose' onClick={()=> setOpen(!open)}/>                    
+
   return (
    
-      <div  onClick={props.handleClick} 
-            className={`icon nav-icon-5 ${props.clicked ? 'open' : ''}`} >
-        <span></span>
-        <span></span>
-        <span></span>
+    <div className="container-button">
+
+{open ? closeIcon : hamburgerIcon} 
+{open &&
+    <div className="botones-burguer">
       
-    </div>
+    <a href="#">Inicio </a>
+    <a href="#">  Servicios</a>
+    <a href="#"> Nosotros</a>
+    <a href="#">    Contactanos</a>
+    <a href="#">  Testimonios </a>
+
+</div>
+}
+</div>
+    
   )
 }
 export default BurguerButton;
--- src/Components/Navbaruno/Navbaruno.jsx
+++ src/Components/Navbaruno/Navbaruno.jsx
@@ -1,57 +1,53 @@
 import React, { useState } from "react";
-import BurguerButton from "./BurguerButton.jsx";
 import "./Navbaruno.css";
 import { useAuth0 } from "@auth0/auth0-react";
+import BurguerButton from "./BurguerButton";
+import './BurguerButton.css';
 
 function Navbaruno() {
   //login Auth0
   const { loginWithRedirect } = useAuth0();
   //Fin login Auth0
 
-  const [clicked, setClicked] = useState(false);
-  const handleClick = () => {
-    //cuando esta true lo pasa a false y vice versa
-    setClicked(!clicked);
-  };
+ 
+
   return (
-    <nav className="NavContainer" id="navbar">
+  
+<div className="container">
+    <div className='container-nav-principal' >
+      
+      
       <div className="logotipo">
+       
         <i class="bi bi-fire"></i>
-        <a  style={{color:'white'}}  href="#">Fisio-Roque</a>
+        <a  href="#">Fisio-Roque</a>
+     
       </div>
 
       <div className="botones">
-        <div className={`links ${clicked ? "active" : ""}`}>
-          <a style={{color:'white'}} onClick={handleClick} href="#">
-            Tienda
-          </a>
-          <a  style={{color:'white'}} onClick={handleClick} href="#">
-            Servicios
-          </a>
-          <a  style={{color:'white'}} onClick={handleClick} href="#">
-            Nosotros
-          </a>
-          <a style={{color:'white'}} onClick={handleClick} href="#">
-            Contactanos
-          </a>
-          <a  style={{color:'white'}} onClick={handleClick} href="#">
-            Testimonios
-          </a>
-        </div>
+      
+          <a href="#">Inicio </a>
+          <a href="#">  Servicios</a>
+          <a href="#"> Nosotros</a>
+          <a href="#">    Contactanos</a>
+          <a href="#">  Testimonios </a>
+      
       </div>
+      
 
       <div className="sesion">
+       
         <button className="loginauth" onClick={() => loginWithRedirect()}>
           {" "}
           <i class="bi bi-door-open"></i> Hola, Iniciar Sesion
         </button>
       </div>
 
-      <div className="burguer">
-        <div clicked={clicked} handleClick={handleClick}></div>
-      </div>
-      <div className={`initial ${clicked ? " active" : ""}`}></div>
-    </nav>
+</div>
+<BurguerButton/>
+
+     </div>
+
   );
 }
 
